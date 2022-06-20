579f0ecd8d6702a3c3ec26fe78f18ddce9b5e3e4
Lordiii <lordiii@lordiii.de>
2022-06-20 21:10:09 +0200

    Test Verilog

--- instructions/instructions.v
+++ instructions/instructions.v
@@ -1,11 +1,14 @@
 `default_nettype none
 
-module instruction_decoder(CLK, CBUS);
+module instruction_decoder(CLK, CBUS, OUT);
 
+reg		TEST;
 reg	[3:0]	GRP;		// 4 Bit Instruction Group
 
 input		CLK;		// CPU Clock
 input	[23:0]	CBUS;		// 24 Bit Instruction Bus
+
+output 		OUT;
 
 wire instruction;
 wire arguments;
@@ -13,6 +16,9 @@ wire arguments;
 assign instruction = CBUS[23:16];
 assign arguments = CBUS[15:0];
 
+assign OUT = TEST;
+
 always @ (posedge CLK) begin
+	TEST = ~TEST;
 end
 endmodule
